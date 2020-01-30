5dfbb4f9cc9bed58b3849b5ef08d2425daa6c1a5
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-30 17:44:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,84 @@
 # Add your code here
+class Owner
+OWNERS = [] 
+attr_accessor :name, :pets 
+attr_reader :species 
+
+def initialize(species)
+  @species = species 
+  OWNERS << self 
+  @pets = {:fishes => [], :dogs => [], :cats => []}
+end 
+
+def self.all ##Do we have to use this if we use a class variable? @@all 
+OWNERS 
+end 
+
+def self.reset_all 
+OWNERS.clear 
+end 
+
+def self.count
+OWNERS.size 
+end
+
+def species 
+  @species 
+end 
+
+def say_species 
+  return "I am a #{species}"
+end 
+  
+def name 
+  @name #where did we set @name = name? 
+end 
+
+def buy_fish(name)
+  pets[:fishes] << Fish.new(name) #how does owner.rb know where fish.rb is? 
+end 
+
+def buy_cat(name)
+  pets[:cats] << Cat.new(name)
+end 
+
+def buy_dog(name)
+  pets[:dogs] << Dog.new(name)
+end 
+
+def walk_dogs
+  pets[:dog].each do |dog|
+    dog.mood = "happy"
+  end 
+end 
+
+def play_with_cats
+  pets[:cats].each do |cat| 
+  cat.mood = "Happy"
+  end 
+end 
+
+def feed_fish
+  pets[:fish].each do |fish|
+  fish.mood = "happy"
+  end 
+end 
+
+def list_pets
+  "I have #{pets[:fishes].count} fish. I have #{pets[:cats].count} cats. I have #{pets[:dogs].count} dogs."
+end 
+
+end #finish Owner 
+
+class Fish 
+attr_reader :name 
+attr_accessor :mood 
+
+def initialize 
+  @name = name 
+  @mood = "nervous"
+end 
+
+end 
+
+let!(:pluto) { Dog.new("Pluto")
