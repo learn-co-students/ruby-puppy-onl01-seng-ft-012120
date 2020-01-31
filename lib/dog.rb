72d4e6302ff3f2e555bce710889fb07283f3ab42
Kyle McKenna <mckennakyler@gmail.com>
2020-01-31 03:54:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,32 +1,32 @@
+require 'pry'
+
 class Dog 
-  attr_accessor :name, :save 
+  attr_accessor :name 
   
   @@all = [] #class variable @@all has an empty array
   
   def initialize (name) #initialize with one arguement name
     @name = name
-    @save = save 
-    @@all << self # return all dogs (<< self)
+    save #save is called inside initialize 
+    #@@all << self # return all dogs (don't need this line,but why?)
   end
-  
+ 
   def self.all #method all, returns each dog's name
-    @@all.each do |dog|
-      puts dog.name
-    end 
+    # @@all.each {|dog| puts "#{dog.name}"}
+    @@all 
   end   
   
-  def self.save
-    @@all << self
-  end 
+  def self.print_all #method print_all, prints out each dog name
+   #@@all.each {|dog_name| puts "#{dog_name.name}"}
+    puts @@all.map{|dog| dog.name}
+  end
   
-  def Dog.clear_all #method clear_all, clears (.clear) all of the Dog array
+  def self.clear_all #method clear_all, clears (.clear) all of the Dog array
     @@all.clear 
   end
   
-  def self.print_all #method print_all, prints out each dog name
-    @@all.each do |dog_name| # @@all.each {|dog_name| puts "#{dog_name.name}"}
-      puts dog_name.name
-    end 
-  end
+  def save #method save adds dog instance to @@all array when called
+    @@all << self
+  end 
   
 end   
