a8a707170226a017e2a08dab7e66a816fc98020c
Diego Vasquez <botellongo69@outlook.com>
2020-01-31 20:39:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  @@all = []
   attr_accessor :name
-  
-  def save
-    @@all << self
-  end
+  @@all = []
   
   def initialize(name)
     @name = name
     save
+  end
+  
+  def save
+    @@all << self
   end
   
   def self.all
@@ -16,10 +16,12 @@ class Dog
   end
   
   def self.print_all
-    
+    @@all.each do |dog|
+      puts dog.name
+    end
   end
   
   def self.clear_all
-    
+    @@all.clear
   end
 end
