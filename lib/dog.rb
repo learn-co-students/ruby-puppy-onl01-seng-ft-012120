6fcfe58040c59c90b24f1b74848bdb7649a2df40
Steven Perfors <stevecanbuild@gmail.com>
2020-02-01 17:43:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+  attr_accessor :name  
+  @@all = []
+  
+  def save
+    @@all << self 
+  end
+  
+  def initialize(name)
+    @name = name 
+    save 
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.print_all
+    self.all.each{|dog| puts "#{dog.name}"}
+  end
+  
+  def self.clear_all
+    self.all.clear 
+  end 
+  
+  
+end
