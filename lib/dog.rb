3b78fec2481de4244344374073956fe2bfe87da3
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-01 09:38:10 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+
+
+
+
+
+
+end
+
+
+pluto = Dog.new("Pluto")
+fido = Dog.new("Fido")
+maddy = Dog.new("Maddy")
