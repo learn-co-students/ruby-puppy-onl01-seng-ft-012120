015f13923cdb52f3c774ac0f8f4b9d6b7cf86742
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-01 10:12:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,35 @@
+
+
 class Dog 
   
   @@all = []
   
-  attr_accessor :name 
+  attr_accessor :name, :save
   
   def initialize(name)
     @name = name
+    @save = save
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear 
+  end
 
-
-
-
-
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+    
+    def save
+      @@all << self
+    end
+    
+  end
 
 end
 
@@ -21,3 +37,5 @@ end
 pluto = Dog.new("Pluto")
 fido = Dog.new("Fido")
 maddy = Dog.new("Maddy")
+
+
